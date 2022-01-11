5ed4d1165cdaa0952eb0a5273e618447dee1c5b7
rkdwlss2 <rkdwlss2@gmail.com>
2022-01-11 23:39:37 +0900

    entity 추가

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -10,10 +10,6 @@ public class JpashopApplication {
 
 
 	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.setData("Hello");
-		String data = hello.getData();
-		System.out.println("data = "+data);
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -12,4 +12,12 @@ public class Address {
     private String street;
     private String zipcode;
 
+    protected Address() { //함부로 생성하지 마세요~
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }

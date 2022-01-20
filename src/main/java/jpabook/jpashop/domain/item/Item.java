86d2c3b5605b0d6a17510ae711bc3ffd125a6213
rkdwlss2 <rkdwlss2@gmail.com>
2022-01-21 00:33:40 +0900

    member test 코드 추가+상품 갯수 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직 ==//
+
+    /**
+     * stock증가 데이터를 가지고 있는 로직에서 데이터를 수정해서 관리하기에 핵심 비즈니스 로직으로 변경
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity-quantity;
+        if (restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
